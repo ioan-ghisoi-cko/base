2f951104064f9c3f6212d233417f262a3357cfa7
ioan.ghisoi95 <ionutghisoi95@gmail.com>
2020-05-24 18:04:28 +0100

    Add Frames

--- src/back-end/api.js
+++ src/back-end/api.js
@@ -1,12 +1,17 @@
 const route = require("express").Router();
+const { Checkout } = require("checkout-sdk-node");
+const cko = new Checkout("sk_test_0b9b5db6-f223-49d0-b68f-f6643dd4f808");
 
-route.get("/getExample", (req, res) => {
-  res.send({ get: "example" });
-});
-
-route.post("/postExample", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+route.post("/payWithToken", async (req, res) => {
+  const payment = await cko.payments.request({
+    source: {
+      token: req.body.token,
+    },
+    currency: "EUR",
+    amount: 1000, // cents
+    reference: "ORDER123",
+  });
+  res.send(payment);
 });
 
 module.exports = route;
--- src/front-end/scripts/script.js
+++ src/front-end/scripts/script.js
@@ -1,7 +1,97 @@
+var payButton = document.getElementById("pay-button");
+var form = document.getElementById("payment-form");
+
+Frames.init("pk_test_4296fd52-efba-4a38-b6ce-cf0d93639d8a");
+
+Frames.addEventHandler(
+  Frames.Events.CARD_VALIDATION_CHANGED,
+  onCardValidationChanged
+);
+function onCardValidationChanged(event) {
+  var errorMessage = document.querySelector(".error-message");
+  payButton.disabled = !Frames.isCardValid();
+}
+
+Frames.addEventHandler(
+  Frames.Events.FRAME_VALIDATION_CHANGED,
+  onValidationChanged
+);
+function onValidationChanged(event) {
+  var errorMessage = document.querySelector(".error-message");
+  errorMessage.textContent = getErrorMessage(event);
+}
+
+var errors = {};
+errors["card-number"] = "Please enter a valid card number";
+errors["expiry-date"] = "Please enter a valid expiry date";
+errors["cvv"] = "Please enter a valid cvv code";
+
+function getErrorMessage(event) {
+  if (event.isValid || event.isEmpty) {
+    return "";
+  }
+
+  return errors[event.element];
+}
+
+Frames.addEventHandler(
+  Frames.Events.CARD_TOKENIZATION_FAILED,
+  onCardTokenizationFailed
+);
+function onCardTokenizationFailed(error) {
+  Frames.enableSubmitForm();
+}
+
+Frames.addEventHandler(Frames.Events.CARD_TOKENIZED, onCardTokenized);
+function onCardTokenized(event) {
+  var el = document.querySelector(".success-payment-message");
+  payWithToken(event.token);
+}
+
+payButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  Frames.submitCard();
+  Frames.enableSubmitForm();
+});
+
+const payWithToken = (token) => {
+  http(
+    {
+      method: "POST",
+      route: "/payWithToken",
+      body: { token: token },
+    },
+    (data) => {
+      console.log("The API RESPONSE: ", data);
+    }
+  );
+};
+
+const http = ({ method, route, body }, callback) => {
+  let requestData = {
+    method,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  };
+
+  if (method.toLocaleLowerCase() === "get") {
+    delete requestData.body;
+  }
+
+  fetch(`${window.location.origin}${route}`, requestData)
+    .then((res) => res.json())
+    .then((data) => callback(data))
+    .catch((er) => console.log(er));
+};
+
+// Socket part:
 var socket = io(window.location.protocol + "//" + window.location.hostname);
 socket.on("connect", function () {
   console.log("socket connected");
 });
-socket.on("webhook", (data) => {
-  alert(data.type);
+socket.on("webhook", (webhookBody) => {
+  console.log("WEBHOOK: ", webhookBody);
 });
